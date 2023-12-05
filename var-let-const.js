22a1137ac6d13d05b58f11c4f2fd49c2821203b3
ardi1304 <ardiandrisuhendri@gmail.com>
2023-12-05 15:05:56 +0700

    pengenalan var,let,const

---
+++ var-let-const.js
@@ -0,0 +1,18 @@
+// mengenal var, let, const
+
+var message = 'hello javascript';
+let msg1 = 'Ardi';
+const msg2 = 'Andri';
+
+message = 'semangat';
+msg1 = 'idra';
+//msg2 = 'irdna';
+
+console.log(msg1);
+console.log(message);
+console.log(msg2);
+
+
+// untuk const nilainya tetap
+// untuk var bisa dirubah dimanapun
+//sedangkan utk let hanya bisa drubah di dalm satu function (satu tempat)
