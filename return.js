b395f1429a6ace30279c89a7ade3b45599157fac
ardi1304 <ardiandrisuhendri@gmail.com>
2023-12-07 11:40:43 +0700

    tugas 3 javascript

---
+++ return.js
@@ -0,0 +1,7 @@
+function nilai(x) {
+    console.log (x = "nilai belum berubah");
+    x = 'nilai sudah berubah';
+    return x;
+}
+console.log(nilai());
+
---
+++ tugas_javascript/tugas-3.js
@@ -0,0 +1,6 @@
+function nilai(x) {
+    console.log (x = 'nilai belum berubah');
+    x = 'nilai sudah berubah';
+    return x;
+}
+console.log(nilai());
