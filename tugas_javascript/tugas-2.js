ae3081b94eb515545b5ebcad3c07232053eb728b
ardi1304 <ardiandrisuhendri@gmail.com>
2023-12-07 09:18:04 +0700

    penambahan folder tugas javascript

---
+++ tugas_javascript/tugas-2.js
@@ -0,0 +1,11 @@
+var nilaiNumber = 13.3;
+var nilaiString = 'Ardi Andri';
+var nilaiBoolean = true;
+var nilaiNull = null;
+var nilaiArray = ['N', 'I', 'O', 'M', 'I', 'C'];
+
+console.log(nilaiNumber);
+console.log(nilaiString);
+console.log(nilaiBoolean);
+console.log(nilaiNull);
+console.log(nilaiArray[5]);
---
+++ tugas_javascript/tugas1.js
@@ -0,0 +1,7 @@
+var nama_depan = "Ardi";
+let nama_akhir = "Andri";
+const umur = 40;
+
+console.log(nama_depan);
+console.log(nama_akhir);
+console.log(umur);
