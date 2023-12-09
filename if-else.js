07527480d60cc793d332d2ecc8785f26580452de
ardi1304 <ardiandrisuhendri@gmail.com>
2023-12-09 09:04:40 +0700

    materi if-else dan mulai tugas-4.js

---
+++ if-else.js
@@ -0,0 +1,9 @@
+function pertimbanganNilai(){
+    nilai = 5;
+    if (nilai===3){
+        console.log('nilai Betul');
+    } else {
+        console.log('nilai Salah');
+    }
+}
+pertimbanganNilai();
