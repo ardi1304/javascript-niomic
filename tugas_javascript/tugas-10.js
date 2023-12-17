f6423434973f06e789db194fb6b58e763c3f7a95
ardi1304 <ardiandrisuhendri@gmail.com>
2023-12-17 13:31:10 +0700

    menambhkan tugas 10 dan 11.js

---
+++ tugas_javascript/tugas-10.js
@@ -0,0 +1,20 @@
+function tugas10(){
+    let bilangan= 1;
+    let penampung = 2;
+    for (let i = 1; i <= 10; i++) {
+        console.log(bilangan);
+        bilangan += penampung;
+        penampung++;
+    }    
+}
+tugas10();
+
+
+// let currentNumber = 1;
+// let increment = 2;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(currentNumber);
+//   currentNumber += increment;
+//   increment++;
+// }
---
+++ tugas_javascript/tugas-11.js
@@ -0,0 +1,5 @@
+function tugas11(){
+    let angka = (10 * 5) / (4 - 2);
+    console.log(angka);
+}
+tugas11();
