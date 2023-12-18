9e8546864dca2263c60295e55254d0735c19f209
ardi1304 <ardiandrisuhendri@gmail.com>
2023-12-18 08:47:19 +0700

    rename tugas-1.js dan memulai tugas-12.js

---
+++ tugas_javascript/tugas-12.js
@@ -0,0 +1,16 @@
+// function tugas12(){
+//     const namaBuah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+//     console.log(namaBuah);
+//     namaBuah.pop();
+//     console.log(namaBuah);
+//     namaBuah.shift();
+//     console.log(namaBuah);
+// }   
+// tugas12();
+
+
+function tugas12(){
+    const namaBuah = ['Pisang', 'Jeruk', 'Apel', 'Mangga'];
+    return namaBuah.pop()
+}   
+console.log(namaBuah);
