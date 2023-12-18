6a496f057763373d5bc73038440698c8ce0c2971
qthinklearn <qthink.learn@gmail.com>
2023-12-18 22:06:34 +0700

    belajar math object dan membuat tugas-14.js

---
+++ math-objectproperty.js
@@ -0,0 +1,8 @@
+console.log('Math.E       = ' + Math.E + ' adalah angka logaritma e');
+console.log('Math.LN10    = ' + Math.LN10 + ' adalah angka logaritma natural 10');
+console.log('Math.LN2     = ' + Math.LN2 + ' adalah angka logaritma natural 2');
+console.log('Math.LOG10E  = ' + Math.LOG10E + '  adalah angka logaritma natural e basis 10');
+console.log('Math.LOG2E   = ' + Math.LOG2E +  '  adalah logaritma natural e basis 2');
+console.log('Math.PI      = ' + Math.PI + '      adalah angka pi 3,14xxxx');
+console.log('Math.SQRT1_2 = ' + Math.SQRT1_2 + ' adalah angka 1 dibagi dengan akar kuadarat 2');
+console.log('Math.SQRT2   = ' + Math.SQRT2 + '   adalah akar kuadrat 2');
---
+++ tugas_javascript/tugas-14.js
@@ -0,0 +1,9 @@
+function tugas14(){
+    let hasilPembagian = 37 / 13;
+    return hasilPembagian;
+}
+console.log(tugas14());
+let hasil = tugas14();
+console.log("Hasil Pembagian (Math.round()): " + Math.round(hasil));
+console.log("Hasil Pembagian (Math.ceil()): " + Math.ceil(hasil));
+console.log("Hasil Pembagian (Math.floor()): " + Math.floor(hasil));
