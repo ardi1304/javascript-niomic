7d2addd5f2592616490a62ec01ede8ac0ca03875
ardi1304 <ardiandrisuhendri@gmail.com>
2024-05-05 15:27:13 +0700

    mengganti file charat.js menjadi belajar_string.js

--- belajar_string.js
+++ belajar_string.js
@@ -22,3 +22,17 @@ function ubah(){
     console.log(huruf.toLocaleUpperCase());
 }
 ubah();
+
+
+//Latihan 23: fungsi char at : menampilkan karakter  tertentu sesuai posisi yang di tentukan
+function posisiKarakter(){
+    var poskrktr = 'Nama Saya Ardi';
+    console.log(poskrktr.charAt(13));
+    console.log(poskrktr.substring(10, 14));
+}
+posisiKarakter();
+
+//Latihan24 Substr : menampilakn karakter sesuai urutan yang di inginkan
+// function substring(){
+//     var 
+// }
