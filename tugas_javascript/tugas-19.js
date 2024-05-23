962ef67aaaf2dd0ce1ab8295fe7ad0bb6a5485b2
ardi1304 <ardiandrisuhendri@gmail.com>
2024-05-23 22:03:50 +0700

    penambahan tugas-19.js

--- tugas_javascript/tugas-19.js
+++ tugas_javascript/tugas-19.js
@@ -8,3 +8,29 @@ var n = 6;
 for (i = 0; i < n; i++) {
     console.log("#".repeat(i+1))
 }
+
+
+function segitiga1(panjang) {
+    let hasil = '';
+    for (let i = 0; i < panjang; i++) {
+        for (let j = 0; j <= i; j++) {
+            hasil += '* ';
+        }
+        hasil += '\n';
+    }
+    return hasil;
+}
+console.log(segitiga1(10));
+
+
+function segitiga2(panjang) {
+    let hasil = '';
+    for (let i = 0; i < panjang; i++) {
+        for (let j = panjang; j > i; j--) {
+            hasil += '* ';
+        }
+        hasil += '\n';
+    }
+    return hasil;
+}
+console.log(segitiga2(10));
